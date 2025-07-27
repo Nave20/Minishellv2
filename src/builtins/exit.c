8c158beedc62ad3170f20f4c2915bd12d79a1643
28 <vpirotti@z1r7p1.42lyon.fr>
2025-07-27 14:42:19 +0200

    hereeeeeeeedocccccccccccccccccccccccccccccccccccccccccccccc

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -19,6 +19,7 @@ void	goodbye_three(t_all *all)
 
 	i = ft_atoi(all->cmd->cmd_tab[1]);
 	rl_clear_history();
+	heredoc_destroyer(all->data);
 	free_env(all->data->env);
 	free_data(all->data);
 	if (all->stdin_save)
@@ -32,6 +33,7 @@ void	goodbye_three(t_all *all)
 void	goodbye_two(t_all *all)
 {
 	rl_clear_history();
+	heredoc_destroyer(all->data);
 	free_env(all->data->env);
 	free_data(all->data);
 	if (all->stdin_save)
@@ -48,6 +50,7 @@ void	goodbye_one(t_all *all)
 
 	i = all->data->err_code;
 	rl_clear_history();
+	heredoc_destroyer(all->data);
 	free_env(all->data->env);
 	free_data(all->data);
 	if (all->stdin_save)
